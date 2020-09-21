633e25754833fedf141bae9542c14d56c25b63d3
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-09-22 01:26:20 +0200

    add examples

---
+++ Examples/Examples1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Examples
+{
+    public class Examples1
+    {
+        private static IEnumerable<int> Fibonacci()
+        {
+            var first = 0;
+            var second = 1;
+
+            yield return first;
+            yield return second;
+
+            while (true)
+            {
+                (first, second) = (second, second + first);
+                yield return second;
+            }
+        }
+    }
+}
