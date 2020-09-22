8a0378518fa5e0f25f9e899aaf526eff362d408a
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-09-22 15:28:36 +0200

    add excercise 2

---
+++ Excercise/Exercise2/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Exercise2
+{
+    public abstract class Person
+    {
+        public virtual void SayHello()
+        {
+            Console.WriteLine("Hello from Person");
+        }
+
+        public abstract void Scream(string words);
+
+        protected virtual void SayProtectedHello()
+        {
+            Console.WriteLine("protected Hello from Person");
+        }
+
+        protected abstract void SecretScream(string words);
+    }
+
+    public class Manager: Person
+    {
+        public override void Scream(string words)
+        {
+            Console.WriteLine("scream from Manager");
+
+        }
+
+        protected override void SecretScream(string words)
+        {
+            Console.WriteLine("secret Scream from ");
+        }
+    }
+}
