d0347e949105d85a9094b04c5fe686f5ac299540
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-09-22 15:30:04 +0200

    implement excercise 3

---
+++ Excercise/Exercise3/ExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Exercise.Exercise1.Car;
+using Exercise.Exercise1.Engine;
+
+namespace Exercise.Exercise3
+{
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<ICar<IEngine>> GetAllCarsByEngine<TEngineType>(this IEnumerable<ICar<IEngine>> cars)
+            where TEngineType : IEngine
+        {
+            return cars.Where(c => c.Engine.GetType() == typeof(TEngineType));
+        }
+
+        public static IEnumerable<ICar<IEngine>> GetAllCarsWithV8Engine(this IEnumerable<ICar<IEngine>> cars)
+        {
+            return cars.Where(c => c.Engine.GetType() == typeof(V8Engine));
+        }
+    }
+}
