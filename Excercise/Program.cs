c2c78aff04fad1f7fc2279d3b03024f74bdae471
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-09-22 15:30:59 +0200

    add calling samples

--- Excercise/Program.cs
+++ Excercise/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using Exercise.Exercise1.Car;
+using Exercise.Exercise1.Engine;
+using Exercise.Exercise3;
 
 namespace Exercise
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            ICar<IEngine> test = new Mustang();
+
+            var cars = new List<ICar<IEngine>>
+            {
+                new Mustang()
+            };
+            var allCarsByEngineNonGeneric = cars.GetAllCarsWithV8Engine();
+            var allCarsByEngine = cars.GetAllCarsByEngine<V8Engine>();
         }
     }
 }
