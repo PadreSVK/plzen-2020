22825e062eabc08b29315ba976737ecef1019557
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-09-23 21:05:23 +0200

    add excercise

--- Excercise/Program.cs
+++ Excercise/Program.cs
@@ -5,6 +5,7 @@ using Exercise.Exercise1.Car;
 using Exercise.Exercise1.Engine;
 using Exercise.Exercise3;
 using Exercise.Exercise5;
+using Exercise.Exercise6;
 
 namespace Exercise
 {
@@ -14,17 +15,17 @@ namespace Exercise
         {
             Console.WriteLine("Hello World!");
 
-            ICar<IEngine> test = new Mustang( new V8Engine("ca412e5e-a731-4b9c-8176-1341035db2fa"));
+            ICar<IEngine> test = new Mustang(new V8Engine("ca412e5e-a731-4b9c-8176-1341035db2fa"));
 
             var cars = new List<ICar<IEngine>>
             {
-                new Mustang(new V8Engine( "a54824fa-3190-412d-846c-ec406cd3cd1d")),
-                new Mustang(new V8Engine( "a54824fa-3190-3190-846c-ec406cd3cd1d")),
-                new Mustang(new V8Engine( "a54824fa-846c-412d-846c-ec406cd3cd1d")),
-                new Mustang(new V8Engine( "a54824fa-846c-3190-846c-ec406cd3cd1d")),
-                new Trabant(new ClassicEngine( "a54824fa-846c-412d-412d-ec406cd3cd1d")),
-                new Trabant(new ClassicEngine( "a54824fa-846c-412d-412d-ec406cd3cd1d")),
-                new Trabant(new ClassicEngine( "a54824fa-846c-412d-412d-ec406cd3cd1d")),
+               new Mustang(new V8Engine( "a54824fa-3190-412d-846c-ec406cd3cd1d")),
+               new Mustang(new V8Engine( "a54824fa-3190-3190-846c-ec406cd3cd1d")),
+               new Mustang(new V8Engine( "a54824fa-846c-412d-846c-ec406cd3cd1d")),
+               new Mustang(new V8Engine( "a54824fa-846c-3190-846c-ec406cd3cd1d")),
+               new Trabant(new ClassicEngine( "a54824fa-846c-412d-412d-ec406cd3cd1d")),
+               new Trabant(new ClassicEngine( "a54824fa-846c-412d-412d-ec406cd3cd1d")),
+               new Trabant(new ClassicEngine( "a54824fa-846c-412d-412d-ec406cd3cd1d")),
             };
 
             var allCarsWithV8Engine = ExtensionMethods.GetAllCarsWithV8Engine(cars);
@@ -37,6 +38,8 @@ namespace Exercise
             {
                 Console.WriteLine(i);
             }
+
+            Extensions.Test();
         }
     }
 }
