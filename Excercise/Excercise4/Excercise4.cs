b5340220ee1e6dba06051b27ac171c4444ea5bec
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-09-23 10:08:00 +0200

    add excercise 4 solution

---
+++ Excercise/Excercise4/Excercise4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Excercise4
+{
+    public class Excercise4
+    {
+        public static IEnumerable<string> GetStrings(int maxNumberOfItems)
+        {
+            for (var i = 0; i < maxNumberOfItems; i++)
+            {
+                yield return $"original string {i}";
+            }
+        }
+    }
+}
