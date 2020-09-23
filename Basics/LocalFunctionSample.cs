58c702459c3ab69b057b28655e916f9912308eef
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-09-23 11:11:01 +0200

    add local funcion sample

---
+++ Basics/LocalFunctionSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    public class LocalFunctionSample: ISample
+    {
+        public void Run()
+        {
+            Console.WriteLine(getAge("asdasdas"));
+
+
+            int age = getElement<int>("age");
+
+            T getElement<T>(string xmlElement)
+            {
+                object value = ""; // getElementValue
+                return (T)value;
+            }
+
+            int getAge(string name) 
+            {
+                return 5;
+            }
+        }
+    }
+}
