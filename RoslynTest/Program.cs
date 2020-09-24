30aec3ebaecd8416ce3400f6b51af5d68a970593
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-09-24 22:19:58 +0200

    add roslyn analyzer sample to solution

---
+++ RoslynTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RoslynTest
+{
+    internal class Program
+    {
+#pragma warning disable VSTHRD200
+        private static async Task Main(string[] args)
+#pragma warning restore VSTHRD200
+        {
+            await TestAsync();
+            Console.WriteLine("Hello World!");
+        }
+
+        private static async Task TestAsync()
+        {
+            var fromResult = await Task.FromResult(1);
+        }
+    }
+}
